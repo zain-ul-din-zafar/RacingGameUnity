55c24930a11abce6c65b714442dfcae21ec579aa
zainuldeen <fa20-bscs-147@lgu.edu.pk>
2022-08-02 09:54:21 -0700

    traffice system from scratch

---
+++ TrafficeSystem/Assets/Scripts/LevelSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSpawner : MonoBehaviour {
+    public static LevelSpawner Instance { get; private set; }
+
+    #region SpawnLogic
+    
+    [SerializeField] private List <GameObject> levels;
+    [SerializeField] private Vector3 spawnPosition;
+    [SerializeField] private Vector3 incrementVector;
+    private int currentSpawnLevel ;
+    private Stack<GameObject> levelsStack;
+
+    private void Awake() {
+        if (Instance != null) Destroy(this);
+        Instance = this;
+        levelsStack = new Stack<GameObject>();
+    }
+    private void Start() { levelsStack.Push(Instantiate(levels[currentSpawnLevel] , spawnPosition , Quaternion.identity)); }    
+    
+    #endregion
+    
+    /// <summary>
+    /// Spawns a new level and destroy previous one. 
+    /// </summary>
+    public void SpawnNextLevel () {
+        Destroy(levelsStack.Pop() , 1f);
+        currentSpawnLevel = (currentSpawnLevel + 1) % levels.Count;
+        spawnPosition += incrementVector;
+        levelsStack.Push(Instantiate(levels[currentSpawnLevel] , spawnPosition , Quaternion.identity));
+    }
+    
+    // Testing
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) SpawnNextLevel();
+    }
+    
+}
---
+++ TrafficeSystem/Assets/Scripts/TrafficSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TrafficSystem {
+     public class TrafficSpawner : MonoBehaviour {
+          [SerializeField] private List<Vehicle> vehicles;
+          [Header("Time interval in sec")][SerializeField] private float spawnInterval = 0.5f;
+          [SerializeField] private float spawnPointIncrement = 1f;
+          
+          private void Start() {
+               IEnumerator coroutine  = Spawn();
+               StartCoroutine(coroutine);
+          }
+          
+          private IEnumerator Spawn() {
+               while (true) {
+                    yield return new WaitForSeconds(spawnInterval);
+                    vehicles.ForEach(vehicle => {
+                         Instantiate(vehicle.vehicle , vehicle.spawnStartPoint.position , Quaternion.identity);
+                         spawnPointIncrement += 1f;
+                    });
+               }
+          }
+     }
+
+     [System.Serializable]
+     public class Vehicle {  
+          public GameObject vehicle;
+          public Transform spawnStartPoint;
+     }
+}
+
+
+
+
