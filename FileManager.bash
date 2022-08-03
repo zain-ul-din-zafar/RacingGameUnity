c2cd8b0fb4f3672ff30caac0595eb3e7acd71120
Zain-ul-din <zain.personal47@gmail.com>
2022-08-03 12:07:32 -0700

    packageJSON

---
+++ FileManager.bash
@@ -0,0 +1,15 @@
+
+# bash script to manage files and directories
+#   - create directory
+#   - create file
+#   - delete directory
+#   - delete file
+#   - move directory
+#   - move file
+#   - copy directory
+#   - copy file
+#   - rename directory                  
+
+
+# not yet implemented 
+
